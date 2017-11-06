8b255a8b5a0dea93f5ef142a598e4d3f545607f5
youngoose <edwardjo811@gmail.com>
2017-11-06 19:35:04 +0000

    add exercise4

---
+++ case.sql
@@ -0,0 +1,55 @@
+SELECT title, released_year,
+       CASE 
+         WHEN released_year >= 2000 THEN 'Modern Lit'
+         ELSE '20th Century Lit'
+       END AS GENRE
+FROM books;
+ 
+SELECT title, stock_quantity,
+    CASE 
+        WHEN stock_quantity BETWEEN 0 AND 50 THEN '*'
+        WHEN stock_quantity BETWEEN 51 AND 100 THEN '**'
+        ELSE '***'
+    END AS STOCK
+FROM books;
+ 
+SELECT title,
+    CASE 
+        WHEN stock_quantity BETWEEN 0 AND 50 THEN '*'
+        WHEN stock_quantity BETWEEN 51 AND 100 THEN '**'
+        ELSE '***'
+    END AS STOCK
+FROM books;
+ 
+SELECT title, stock_quantity,
+    CASE 
+        WHEN stock_quantity BETWEEN 0 AND 50 THEN '*'
+        WHEN stock_quantity BETWEEN 51 AND 100 THEN '**'
+        WHEN stock_quantity BETWEEN 101 AND 150 THEN '***'
+        ELSE '****'
+    END AS STOCK
+FROM books;
+ 
+SELECT title, stock_quantity,
+    CASE 
+        WHEN stock_quantity <= 50 THEN '*'
+        WHEN stock_quantity <= 100 THEN '**'
+        ELSE '***'
+    END AS STOCK
+FROM books; 
+
+SELECT title, author_lname,
+    CASE 
+        WHEN title LIKE '%stories%' THEN 'Short Stories'
+        WHEN title = 'Just Kids' OR title = 'A Heartbreaking Work of Staggering Genius' THEN 'Momoir'
+        ELSE 'Novel'
+    END AS TYPE
+FROM books;
+
+SELECT author_lname,
+    CASE 
+        WHEN COUNT(*) = 1 THEN '1 book'
+        ELSE CONCAT(COUNT(*), ' books')
+    END AS COUNT
+FROM books
+GROUP BY author_lname, author_fname;
---
+++ logical.sql
@@ -0,0 +1,52 @@
+SELECT title, author_lname, released_year FROM books
+WHERE author_lname='Eggers';
+ 
+SELECT title, author_lname, released_year FROM books
+WHERE released_year > 2010;
+ 
+SELECT  
+    title, 
+    author_lname, 
+    released_year FROM books
+WHERE author_lname='Eggers' 
+    AND released_year > 2010;
+ 
+SELECT 1 < 5 && 7 = 9;
+-- false
+ 
+SELECT -10 > -20 && 0 <= 0;
+-- true
+ 
+SELECT -40 <= 0 AND 10 > 40;
+--false
+ 
+SELECT 54 <= 54 && 'a' = 'A';
+-- true
+ 
+SELECT 
+    title, 
+    author_lname, 
+    released_year 
+FROM books
+WHERE author_lname='Eggers' || released_year > 2010;
+ 
+ 
+SELECT 40 <= 100 || -2 > 0;
+-- true
+ 
+SELECT 10 > 5 || 5 = 5;
+-- true
+ 
+SELECT 'a' = 5 || 3000 > 2000;
+-- true
+ 
+SELECT title, 
+       author_lname, 
+       released_year, 
+       stock_quantity 
+FROM   books 
+WHERE  author_lname = 'Eggers' 
+              || released_year > 2010 
+OR     stock_quantity > 100;
+    
+
