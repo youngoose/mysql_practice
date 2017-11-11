79c571f25ea41426db28eb7a5e8b5fc2346b1552
youngoose <edwardjo811@gmail.com>
2017-11-11 15:37:56 +0000

    add one to many

--- Relationships/CustomersOrders.sql
+++ Relationships/CustomersOrders.sql
@@ -50,7 +50,6 @@ SELECT first_name, last_name, order_date, amount
 FROM customers, orders 
     WHERE customers.id = orders.customer_id;
     
-    
 -- EXPLICIT INNER JOINS
 SELECT * FROM customers
 JOIN orders
@@ -70,3 +69,46 @@ JOIN customers
 -- ARBITRARY JOIN - meaningless, but still possible 
 SELECT * FROM customers
 JOIN orders ON customers.id = orders.id;
+
+-- GETTING FANCIER
+SELECT first_name, 
+       last_name, 
+       order_date, 
+       SUM(amount) AS total_spent
+FROM customers
+JOIN orders
+    ON customers.id = orders.customer_id
+GROUP BY orders.customer_id
+ORDER BY total_spent;
+
+-- LEFT JOIN
+SELECT * FROM customers
+LEFT JOIN orders
+    ON customers.id = orders.customer_id;
+    
+SELECT first_name, last_name, order_date, amount 
+FROM customers
+LEFT JOIN orders
+    ON customers.id = orders.customer_id;
+    
+SELECT first_name, last_name, order_date, amount 
+FROM customers
+LEFT JOIN orders
+    ON customers.id = orders.customer_id
+GROUP By customers.id;
+
+SELECT 
+    first_name, 
+    last_name,  
+    IFNULL(SUM(amount), 0) AS total_spent
+FROM customers
+LEFT JOIN orders
+    ON customers.id = orders.customer_id
+GROUP By customers.id
+ORDER BY total_spent;
+
+-- RIGHT JOIN
+SELECT * FROM customers
+RIGHT JOIN orders
+    ON customers.id = orders.customer_id;
+    
